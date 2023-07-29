ac96cc3953e3238a5526d16322ba0aad9c10bf38
saraswat <kamalsaraswat8@gmail.com>
2023-07-30 00:16:51 +0530

    init commit

---
+++ components/navbar.js
@@ -0,0 +1,18 @@
+const navbar = () => {
+    return `        <div class="nav">
+    <div><a href="./index.html"><i class="fa fa-home" aria-hidden="true" style="font-size:40px;"></i></a></div>
+
+    <ul>
+       
+        <li><a href="wallet.html">Wallet</a></li>
+        <li><a href="like.html">Favorite</a></li>
+        <li><a href="cart.html">Cart</a><sup></sup></li>
+        <li><a href="login.html">Login</a></li>
+        <li><a href="signup.html">SignUp</a></li>
+    </ul>
+</div>`
+
+}
+
+export default navbar;
+
